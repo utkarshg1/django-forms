e572a43fd5c6c1f0359d89edc4f1113ab6fa3607
utkarshg1 <gaikwadujg@gmail.com>
2024-09-25 15:28:26 +0530

    ListView and DetailView

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,7 +1,12 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.views import View
+from django.views.generic.base import TemplateView
+from django.views.generic import ListView, DetailView
 from .forms import ReviewForm
+from .models import Review
 
 
 class ReviewView(View):
@@ -24,6 +29,25 @@ class ReviewView(View):
         })
 
 
-class ThankYouView(View):
-    def get(self, request):
-        return render(request, "reviews/thank_you.html")
+class ThankYouView(TemplateView):
+    template_name = "reviews/thank_you.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["message"] = "This works!"
+        return context
+
+
+class ReviewListView(ListView):
+    template_name = "reviews/review_list.html"
+    model = Review
+    context_object_name = "reviews"
+
+    # def get_queryset(self):
+    #     base_queryset = super().get_queryset()
+    #     return base_queryset.filter(rating__gt=4)
+
+
+class SingleReviewView(DetailView):
+    template_name = "reviews/single_review.html"
+    model = Review
