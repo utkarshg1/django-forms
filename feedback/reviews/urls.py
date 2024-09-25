f63a39919b30af1c29542cc891f2df0464d5bfa5
utkarshg1 <gaikwadujg@gmail.com>
2024-09-26 00:58:29 +0530

    Sessions done

--- feedback/reviews/urls.py
+++ feedback/reviews/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("", views.ReviewView.as_view()),
     path("thank-you", views.ThankYouView.as_view()),
     path("reviews", views.ReviewListView.as_view()),
+    path("reviews/favourite", views.AddFavouriteView.as_view()),
     path("reviews/<int:pk>", views.SingleReviewView.as_view())
 ]
--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,6 +1,8 @@
+from django.views import View
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView
+from django.http import HttpResponseRedirect
 from .forms import ReviewForm
 from .models import Review
 
@@ -52,3 +54,18 @@ class ReviewListView(ListView):
 class SingleReviewView(DetailView):
     template_name = "reviews/single_review.html"
     model = Review
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        loaded_review = self.object
+        request = self.request
+        favourite_id = request.session.get("favourite_review")
+        context["is_favourite"] = favourite_id == str(loaded_review.id)
+        return context
+
+
+class AddFavouriteView(View):
+    def post(self, request):
+        review_id = request.POST["review_id"]
+        request.session["favourite_review"] = review_id
+        return HttpResponseRedirect(f"/reviews/{review_id}")
